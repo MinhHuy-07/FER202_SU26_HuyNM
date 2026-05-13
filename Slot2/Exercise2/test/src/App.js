5971a1e16c3a768cdd65781aeed923365a5c2468
MinhHuy-07 <xvcxhuy1239999@gmail.com>
2026-05-13 15:58:15 +0700

    Slot 2\Exercise\Slot1

---
+++ Slot2/Exercise2/test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "HuyNM";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello, {name}!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
